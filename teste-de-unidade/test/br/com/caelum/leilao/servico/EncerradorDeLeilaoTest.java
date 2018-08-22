01d90ed693427604861a93e82233d04f3841f79c
XI328883 <p000rbertoni@prservicos.com.br>
2018-08-22 00:19:47 -0300

    Simulando comportamentos com mock objects

---
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -0,0 +1,74 @@
+package br.com.caelum.leilao.servico;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
+
+public class EncerradorDeLeilaoTest {
+	
+	@Test
+	public void deveEncerrarLeiloesQueComecaramAMaisDeUmaSemana() {
+		Calendar antiga = Calendar.getInstance();
+        antiga.set(1999, 1, 20);
+
+        Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma")
+            .naData(antiga).constroi();
+        Leilao leilao2 = new CriadorDeLeilao().para("Geladeira")
+            .naData(antiga).constroi();
+
+        RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+        when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+        EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+        encerrador.encerra();
+
+        assertEquals(2, encerrador.getTotalEncerrados());
+        assertTrue(leilao1.isEncerrado());
+        assertTrue(leilao2.isEncerrado());
+	}
+	
+	@Test
+	public void naoDeveEncerrarLeilaoIniciadoOntem() {
+		Calendar ontem = Calendar.getInstance();
+		ontem.set(2018, 9, 20);
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("Video game")
+				.naData(ontem).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Patins")
+				.naData(ontem).constroi();
+		
+		RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1,leilao2));
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		encerrador.encerra();
+		
+		assertEquals(0, encerrador.getTotalEncerrados());
+		assertFalse(leilao1.isEncerrado());
+		assertFalse(leilao2.isEncerrado());
+	}
+	
+	@Test
+	public void casoNaoHajaLeilaoEncerradorNaoDeveFazerNada() {
+		RepositorioDeLeiloes daoFalso = mock(RepositorioDeLeiloes.class);
+		when(daoFalso.correntes()).thenReturn(new ArrayList<Leilao>());
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		encerrador.encerra();
+		
+		assertEquals(0, encerrador.getTotalEncerrados());
+	}
+
+}
