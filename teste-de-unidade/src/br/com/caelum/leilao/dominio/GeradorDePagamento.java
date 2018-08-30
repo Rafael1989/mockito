11d605e3a7265737a6fbca2f4edf0e44552585ff
rafael <flns_rafa@hotmail.com>
2018-08-30 01:10:09 -0300

    Isolando para testar

--- teste-de-unidade/src/br/com/caelum/leilao/dominio/GeradorDePagamento.java
+++ teste-de-unidade/src/br/com/caelum/leilao/dominio/GeradorDePagamento.java
@@ -9,26 +9,42 @@ import br.com.caelum.leilao.servico.Avaliador;
 public class GeradorDePagamento {
 
 	private final RepositorioDePagamentos pagamentos;
-    private final RepositorioDeLeiloes leiloes;
-    private final Avaliador avaliador;
+	private final RepositorioDeLeiloes leiloes;
+	private final Avaliador avaliador;
+	private Relogio relogio;
 
-    public GeradorDePagamento(RepositorioDeLeiloes leiloes, 
-            RepositorioDePagamentos pagamentos, 
-            Avaliador avaliador) {
-        this.leiloes = leiloes;
-        this.pagamentos = pagamentos;
-        this.avaliador = avaliador;
-    }
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador,
+			Relogio relogio) {
+		this.leiloes = leiloes;
+		this.pagamentos = pagamentos;
+		this.avaliador = avaliador;
+		this.relogio = relogio;
+	}
 
-    public void gera() {
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador) {
+		this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
+	}
 
-        List<Leilao> leiloesEncerrados = leiloes.encerrados();
-        for(Leilao leilao : leiloesEncerrados) {
-            avaliador.avalia(leilao);
+	public void gera() {
 
-            Pagamento novoPagamento = 
-                    new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
-            pagamentos.salva(novoPagamento);
-        }
-    }
+		List<Leilao> leiloesEncerrados = leiloes.encerrados();
+		for (Leilao leilao : leiloesEncerrados) {
+			avaliador.avalia(leilao);
+
+			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), primeiroDiaUtil());
+			pagamentos.salva(novoPagamento);
+		}
+	}
+
+	private Calendar primeiroDiaUtil() {
+		Calendar data = relogio.hoje();
+		int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+
+		if (diaDaSemana == Calendar.SATURDAY)
+			data.add(Calendar.DAY_OF_MONTH, 2);
+		else if (diaDaSemana == Calendar.SUNDAY)
+			data.add(Calendar.DAY_OF_MONTH, 1);
+
+		return data;
+	}
 }
--- teste-de-unidade/test/br/com/caelum/leilao/dominio/GeradorDePagamentoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/dominio/GeradorDePagamentoTest.java
@@ -2,10 +2,11 @@ package br.com.caelum.leilao.dominio;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -15,13 +16,67 @@ import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.servico.Avaliador;
 
 public class GeradorDePagamentoTest {
+
+	@Test
+	public void deveGerarPagamentoParaUmLeilaoEncerrado() {
+
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+		Avaliador avaliador = mock(Avaliador.class);
+
+		Leilao leilao = new CriadorDeLeilao().para("Playstation").lance(new Usuario("José da Silva"), 2000.0)
+				.lance(new Usuario("Maria Pereira"), 2500.0).constroi();
+
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+		when(avaliador.getMaiorLance()).thenReturn(2500.0);
+
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, avaliador);
+		gerador.gera();
+
+		ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		Pagamento pagamentoGerado = argumento.getValue();
+		assertEquals(2500.0, pagamentoGerado.getValor(), 0.00001);
+	}
+
+	@Test
+	public void deveEmpurrarParaOProximoDiaUtil() {
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+		Relogio relogio = mock(Relogio.class);
+
+		// dia 7/abril/2012 eh um sabado
+		Calendar sabado = Calendar.getInstance();
+		sabado.set(2012, Calendar.APRIL, 7);
+
+		// ensinamos o mock a dizer que "hoje" é sabado!
+		when(relogio.hoje()).thenReturn(sabado);
+
+		Leilao leilao = new CriadorDeLeilao().para("Playstation").lance(new Usuario("José da Silva"), 2000.0)
+				.lance(new Usuario("Maria Pereira"), 2500.0).constroi();
+
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
+		gerador.gera();
+
+		ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		Pagamento pagamentoGerado = argumento.getValue();
+
+		assertEquals(Calendar.MONDAY, pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
+		assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
+	}
 	
 	@Test
-    public void deveGerarPagamentoParaUmLeilaoEncerrado() {
-
+    public void deveEmpurrarPagamentoNoDomingoParaOProximoDiaUtil() {
         RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
         RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
-        Avaliador avaliador = mock(Avaliador.class);
+        Relogio relogio = mock(Relogio.class);
+
+        Calendar domingo = Calendar.getInstance();
+        domingo.set(2012, Calendar.APRIL, 8);
+        when(relogio.hoje()).thenReturn(domingo);
 
         Leilao leilao = new CriadorDeLeilao()
             .para("Playstation")
@@ -30,16 +85,19 @@ public class GeradorDePagamentoTest {
             .constroi();
 
         when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
-        when(avaliador.getMaiorLance()).thenReturn(2500.0);
 
         GeradorDePagamento gerador = 
-            new GeradorDePagamento(leiloes, pagamentos, avaliador);
+            new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
         gerador.gera();
 
         ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
         verify(pagamentos).salva(argumento.capture());
         Pagamento pagamentoGerado = argumento.getValue();
-        assertEquals(2500.0, pagamentoGerado.getValor(), 0.00001);
+
+        assertEquals(Calendar.MONDAY, 
+            pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
+        assertEquals(9, 
+            pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
     }
 
 }
